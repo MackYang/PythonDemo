4829d736b6fd0de3ea7202a2a1982335624be3aa
YangJiaYong <yangjiayong@cn.wilmar-intl.cn>
2021-01-11 18:09:04 +0800

    json test

--- json.py
+++ json.py
@@ -1,14 +1,42 @@
 import json
-data = json.dumps([1, 2, 3])
-print(data)
+# data = json.dumps([1, 2, 3])
+# print(data)
 
 class Test:
     def __init__(self, name, age):
         self.name = name
         self.age=age
     
+    def fromDic(self,dct):
+        return Test(dct['name'], dct['age'])
+
+class TestEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Test):
+            return {'name':obj.name, 'age':obj.age}
+        # Let the base class default method raise the TypeError
+        return json.JSONEncoder.default(self, obj)
+
+
 test = Test('aaa', 12)
-encoder=json.encoder.JSONEncoder()
-objData = json.dump(test)
-print(objData)
+encoder = TestEncoder()
+t = json.dumps(test, cls=TestEncoder)
+print(t)
+# dic =TestEncoder.encode(test)
+# print(dic)
+# newObj = test.fromDic(dic)
+# print(newObj)
 #json.dumps([1, 'simple', 'list'])
+
+
+
+
+ 
+
+
+
+
+# json.loads('{"__complex__": true, "real": 1, "imag": 2}',
+#     object_hook=as_complex)
+
+# list(ComplexEncoder().iterencode(2 + 1j))
