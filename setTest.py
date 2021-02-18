a35305754887179ed0b8d851e02dbaf23a9d58fc
YangJiaYong <yangjiayong@cn.wilmar-intl.cn>
2021-02-18 16:15:01 +0800

    test

--- setTest.py
+++ setTest.py
@@ -1,19 +1,16 @@
+#需求:我想把arr中的数字分别放在setA,setB中,A和B中的数字在多线程执行完后也不能相同
+
 import random
 import threading
-from threading import RLock
-from threading import Lock
 
 class A:
     setA = set()
     setB = set()
     arr = [1, 2, 3, 4, 5,6,7,8,9,0]
     threads = []
-    lock=RLock()
     
     def fun(self, condition):
-        
         with condition:
-            condition.notify()
             condition.wait()
             for i in self.arr:
                 r = random.randrange(1, 100)
@@ -23,26 +20,9 @@ class A:
                     self.setB.add(i)
             condition.notify()
     
-    def fun2(self):
-        self.lock.acquire()
-        for i in self.arr:
-            r = random.randrange(1, 10)
-            if (r % 2 == 0):
-                self.setA.add(i)
-                if (i in self.setB):
-                    self.setB.remove(i)
-                    print(f'线程{threading.current_thread().name}移除{i}')
-            elif(not i in self.setA):
-                self.setB.add(i)
-                print(f'线程{threading.current_thread().name}添加{i}')
-        self.lock.release()
-
-    
     def tFun(self):
-        lock = Lock()
-        condition = threading.Condition(lock)
+        condition = threading.Condition()
         for i in range(10):
-            
             t = threading.Thread(target=self.fun, args=(condition,))
             self.threads.append(t)
             t.start()
@@ -51,17 +31,8 @@ class A:
         print(self.setA)
         print(self.setB)
 
-    def tFun2(self):
-        for i in range(10):
-            t = threading.Thread(target=self.fun2)
-            self.threads.append(t)
-            t.start()
-        for t in self.threads:
-            t.join()
-        print(self.setA)
-        print(self.setB)
-
+    
 a = A()
-a.tFun2()
+a.tFun()
 
 
